7dceb439ee4979026543dd3fddcca4fdfffbc25e
karthikeyan <karthikeyanmadhappan@gmail.com>
2025-12-29 12:15:40 +0530

    done

---
+++ React/React-Day-1/src/App.jsx
@@ -0,0 +1,20 @@
+
+import About from './About'
+import './App.css'
+import Contact from './Contact'
+import Home from './Home'
+
+function App() {
+
+
+  return (
+    <>
+     <Home />
+     <About />
+     <Contact />
+     
+    </>
+  )
+}
+
+export default App
---
+++ React/React-Day-1/src/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <div>
+        <ul>
+            <li>Coffee</li>
+            <li>Tea</li>
+            <li>Milk</li>
+        </ul>
+    </div>
+    </>
+  )
+}
+
+export default Home
